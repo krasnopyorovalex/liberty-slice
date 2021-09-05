6c2d06f5bd4e5a65eb253e2e7db61a4888707231
Sanek <sanek@localhost.localdomain>
2021-09-05 08:53:11 +0300

    fix change

--- src/js/index.js
+++ src/js/index.js
@@ -7,3 +7,48 @@ new WOW().init();
 lightbox.option({
     'albumLabel': 'Изображение %1 из %2'
 });
+
+$.ajaxSetup({
+    headers: {
+        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+    }
+});
+
+
+$(document).ready(function(){
+    formHandler('form.form-cost-calculation');
+});
+
+function formHandler(selector) {
+    return $(document).on("submit", selector, function(e){
+        e.preventDefault();
+        const _this = $(this),
+            url = _this.attr('action'),
+            data = _this.serialize(),
+            submitBlock = _this.find("button[type=submit]");
+
+        if (submitBlock.hasClass('is-sent')) {
+            return false;
+        }
+
+        return $.ajax({
+            type: "POST",
+            dataType: "json",
+            url: url,
+            data: data,
+            beforeSend: function() {
+                return submitBlock.addClass("is-sent");
+            },
+            error: function ({responseJSON}) {
+                console.log(responseJSON.message)
+                //return _this.append(data.message);
+            },
+            success: function (response) {
+                console.log(JSON.parse(response))
+                _this.append(response.message);
+                setTimeout(() => $('.success-message').remove(), 5000);
+                return submitBlock.removeClass("is-sent") && _this.trigger("reset");
+            }
+        });
+    });
+}
