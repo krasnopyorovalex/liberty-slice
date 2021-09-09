c88b56c9c3783a456189bc6fb4e81a2869382803
Санек <sanek@localhost.localdomain>
2021-09-09 09:59:20 +0300

    commit changes

--- src/blocks/modules/header/header.js
+++ src/blocks/modules/header/header.js
@@ -6,6 +6,7 @@ $(document).ready(function(){
     const menu = header.find('.menu-line > ul');
     const body = $('body');
     const iconFavorite = $('svg.icon-favorite');
+    const favoriteAction = $('.favorite-action');
 
     if (burgerMob.length) {
         burgerMob.on('click', function () {
@@ -32,5 +33,36 @@ $(document).ready(function(){
         mouseleave: function () {
             return $(this).html(`<use xlink:href="../img/sprites/sprite.svg#favorite"></use>`);
         }
+    });
+
+    favoriteAction.on('click', 'svg', function () {
+        const _this = $(this);
+        let action = _this.closest('.favorite-action').attr('data-action');
+        return $.ajax({
+            type: "POST",
+            dataType: "json",
+            url: action,
+            data: {
+                'entity': $(this).closest('.favorite-action').attr('data-entity')
+            },
+            error: function ({responseJSON}) {
+
+            },
+            success: function ({message}) {
+                body.append(message);
+
+                if (action.indexOf('add') !== -1) {
+                    action = action.replace('add', 'remove');
+                    _this.html(`<use xlink:href="../img/sprites/sprite.svg#favorite-active"></use>`);
+                } else {
+                    action = action.replace('remove', 'add');
+                    _this.html(`<use xlink:href="../img/sprites/sprite.svg#favorite"></use>`);
+                }
+
+                _this.closest('.favorite-action').attr('data-action', action);
+
+                return setTimeout(() => $('.success-message').remove(), 5000);
+            }
+        });
     });
 });
