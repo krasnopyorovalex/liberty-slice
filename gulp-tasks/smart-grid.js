ea28069194497f40fd57324baf9ba86d0757450a
Alexander Krasnoperov <a.krasnoperov@ottoruss.ru>
2021-12-12 15:36:40 +0300

    add grid, and fix menu by notebooks

--- gulp-tasks/smart-grid.js
+++ gulp-tasks/smart-grid.js
@@ -14,23 +14,30 @@ gulp.task("smart-grid", (cb) => {
             container: "container"
         },
         container: {
-            fields: "0.9375rem" // side fields - 15px
+            fields: "30px" // side fields - 15px
         },
         breakPoints: {
             xs: {
-                width: "20rem" // 320px
+                width: "20rem", // 320px
+                fields: '10px'
             },
             sm: {
-                width: "36rem" // 576px
+                width: "36rem", // 576px
+                fields: '15px'
             },
             md: {
-                width: "48rem" // 768px
+                width: "48rem", // 768px
+                fields: '15px'
             },
             lg: {
-                width: "62rem" // 992px
+                width: "62rem", // 992px,
+                fields: '30px'
             },
             xl: {
                 width: "75rem" // 1200px
+            },
+            xxl: {
+                width: "1560px"
             }
         }
     });
